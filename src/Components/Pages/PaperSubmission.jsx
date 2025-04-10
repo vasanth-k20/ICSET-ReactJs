21ae85a88766595d1605253904c6e1fac88ac779
vasanth-k20 <vasanthvvkk03@gmail.com>
2025-04-10 23:43:10 +0530

    papersubmit

--- src/Components/Pages/PaperSubmission.jsx
+++ src/Components/Pages/PaperSubmission.jsx
@@ -55,7 +55,7 @@ export default function PaperSubmission() {
         formDataToSend.append('file', formData.PaperFile);
     
         try {
-            const response = await fetch('http://localhost:5000/api/submit-paper', {
+            const response = await fetch('https://icscent.com/papersubmit.php', {
                 method: 'POST',
                 body: formDataToSend,
             });
